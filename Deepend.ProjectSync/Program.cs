d309167331ae4db2569ae61b66ec68d1699e229a
Peter Dolkens <github@ddrit.com>
2013-10-04 00:47:10 +1000

    Update Program.cs

--- Deepend.ProjectSync/Program.cs
+++ Deepend.ProjectSync/Program.cs
@@ -11,22 +11,29 @@ namespace Deepend.ProjectSync
 {
     class Program
     {
+        private enum ReturnCode {
+            0 = Success,
+            2 = ProjectFileNotFound
+        };
+        
         static Boolean _running = true;
 
-        static void Main(string[] args)
+        static Int32 Main(string[] args)
         {
 // #if DEBUG
 //             args = new String[] { @"D:\Workspaces\carnival\cruise-personaliser\CruiseControl.Web" };
 // #endif
             // Configuration Options
-            String[] s_excludeFilter        = (ConfigurationManager.AppSettings["Filter.Exclude"] ?? @"^config\.rb$|^bin\\|^.sass-cache\\|^obj\\|\.orig$|\\_compiler\\|\.bat$|\.user$|\.csproj$|\.zip$|\.bak$|\.mdf$|\.log$|\.exe$").Split('|');
-            String[] s_noneFilter           = (ConfigurationManager.AppSettings["Filter.None"]    ?? @"\.scss$|\.pubxml$|\\_GUIDES\\").Split('|');
-            String[] s_compileFilter        = (ConfigurationManager.AppSettings["Filter.Compile"] ?? @"\.cs$|\.cshtml$|\.aspx$|\.config$|\.asax$|\.ascx$|\.ashx$").Split('|');
-            String[] s_contentFilter        = (ConfigurationManager.AppSettings["Filter.Content"] ?? @"").Split('|');
-            String defaultContentType       = (ConfigurationManager.AppSettings["DefaultType"]    ?? "Content");
-            Int32 pollingPeriod  = Int32.Parse(ConfigurationManager.AppSettings["PollingPeriod"]  ?? "0");
-            Boolean takeBackup = Boolean.Parse(ConfigurationManager.AppSettings["WriteBackup"]    ?? "true");
+            String[] s_excludeFilter          = (ConfigurationManager.AppSettings["Filter.Exclude"] ?? @"^config\.rb$|^bin\\|^.sass-cache\\|^obj\\|\.orig$|\\_compiler\\|\.bat$|\.user$|\.csproj$|\.zip$|\.bak$|\.mdf$|\.log$|\.exe$").Split('|');
+            String[] s_noneFilter             = (ConfigurationManager.AppSettings["Filter.None"]    ?? @"\.scss$|\.pubxml$|\\_GUIDES\\").Split('|');
+            String[] s_compileFilter          = (ConfigurationManager.AppSettings["Filter.Compile"] ?? @"\.cs$|\.cshtml$|\.aspx$|\.config$|\.asax$|\.ascx$|\.ashx$").Split('|');
+            String[] s_contentFilter          = (ConfigurationManager.AppSettings["Filter.Content"] ?? @"").Split('|');
+            String defaultContentType         = (ConfigurationManager.AppSettings["DefaultType"]    ?? "Content");
+            Int32 pollingPeriod    = Int32.Parse(ConfigurationManager.AppSettings["PollingPeriod"]  ?? "0");
+            Boolean takeBackup   = Boolean.Parse(ConfigurationManager.AppSettings["WriteBackup"]    ?? "true");
+            Boolean autoOrganize = Boolean.Parse(ConfigurationManager.AppSettings["AutoOrganize"]   ?? "true");
 
+            // TODO: Better CLI argument processing - is there a nuget package for this, or should I use my old array->ToDictionary method?
             // Commend Line Arguments
             String rootPath = (args.Length == 1 && Directory.Exists(args[0])) ? args[0] : Environment.CurrentDirectory;
 
@@ -37,7 +44,7 @@ namespace Deepend.ProjectSync
 
             // Parsing config
             Regex[] excludeFilter = s_excludeFilter.Select(x => new Regex(x, RegexOptions.IgnoreCase)).ToArray();
-            Regex[] noneFilter = s_noneFilter.Select(x => new Regex(x, RegexOptions.IgnoreCase)).ToArray();
+            Regex[] noneFilter    = s_noneFilter.Select(x => new Regex(x, RegexOptions.IgnoreCase)).ToArray();
             Regex[] compileFilter = s_compileFilter.Select(x => new Regex(x, RegexOptions.IgnoreCase)).ToArray();
             Regex[] contentFilter = s_contentFilter.Select(x => new Regex(x, RegexOptions.IgnoreCase)).ToArray();
 
@@ -49,8 +56,10 @@ namespace Deepend.ProjectSync
             String[] projFiles = Directory.GetFiles(rootPath, "*.csproj", SearchOption.TopDirectoryOnly);
 
             if (projFiles.Length != 1)
-                // TODO: Print Help
-                throw new Exception("Unable to determine project file");
+            {
+                Console.WriteLine("ProjectSync only supports a single .csproj file per root");
+                return (Int32)ReturnCode.ProjectFileNotFound;
+            }
 
             FileStream projectStream;
 
@@ -73,7 +82,9 @@ namespace Deepend.ProjectSync
 
                     XmlNamespaceManager mgr = new XmlNamespaceManager(doc.NameTable);
                     mgr.AddNamespace("x", "http://schemas.microsoft.com/developer/msbuild/2003");
-
+                    
+                    XmlNodeList allNodes = doc.SelectNodes("//x:ItemGroup/*", mgr);
+                    XmlNodeList referenceNodes = doc.SelectNodes("//x:ItemGroup/Reference", mgr);
                     XmlNodeList noneNodes = doc.SelectNodes("//x:ItemGroup/x:None", mgr);
                     XmlNodeList contentNodes = doc.SelectNodes("//x:ItemGroup/x:Content", mgr);
                     XmlNodeList compileNodes = doc.SelectNodes("//x:ItemGroup/x:Compile", mgr);
@@ -159,9 +170,34 @@ namespace Deepend.ProjectSync
                         remove.ParentNode.RemoveChild(remove);
                     }
 
-                    // TODO: Reorder Nodes - Group Content Types Into ItemGroups
-                    if (modified)
-                    {
+                    if (modified) {
+                        
+                        if (autoOrganize) {
+                            // itemGroups[0] == References
+                            // itemGroups[1] == Compile
+                            // itemGroups[2] == Content
+                            // itemGroups[3] == Other
+                            // itemGroups[4] == None
+                            if (itemGroups.Count > 5) {
+                                // Loop through all XmlNodes and append to itemGroups[3]
+                                
+                                // Delete all itemGroups beyond itemGroups[4]
+                            }
+                            else if (itemGroups.Count < 5) {
+                                // Append itemGroup nodes to itemGroups[0].Parent
+                                
+                                // Loop through all XmlNodes and append to itemGroups[3]
+                            }
+                            else {
+                                // Loop through all XmlNodes and append to itemGroups[3]
+                            }
+                            
+                            // Loop through all Reference nodes and append to itemGroups[0]
+                            // Loop through all Compile nodes and append to itemGroups[1]
+                            // Loop through all Content nodes and append to itemGroups[2]
+                            // Loop through all None nodes and append to itemGroups[4]
+                        }
+                        
                         if (takeBackup)
                         {
                             projectStream.Unlock(0, projectStream.Length);
@@ -193,6 +229,8 @@ namespace Deepend.ProjectSync
             }
 
             if (pollingPeriod != 0) Console.ReadKey();
+            
+            return (Int32)ReturnCode.Success;
         }
     }
 }
